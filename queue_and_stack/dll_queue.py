65f428b1428413e371f66621a558d0e09f8e281a
clem9281 <clem9281@gmail.com>
2019-10-13 12:51:06 -0700

    fixes redundant size/length property in queue and stack

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -8,25 +8,24 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         self.storage = DoublyLinkedList()
-        self.length = 0
     def __str__(self):
         return f"Queue: storage: {self.storage}"
         
     def enqueue(self, value):
         self.storage.add_to_tail(value)
-        self.length += 1
+        self.size += 1
         # pass
 
     def dequeue(self):
-        if self.length is 0:
+        if self.size is 0:
             return
         old_head = self.storage.head
         self.storage.remove_from_head()
-        self.length -= 1
+        self.size -= 1
         return old_head.value
 
     def len(self):
-        return self.length
+        return self.size
 
 q = Queue()
 q.enqueue(4)
